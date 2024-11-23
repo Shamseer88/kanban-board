57ec8fb77d2c912aa4db027ff1062f5156665ec1
shamseer8877 <alishamseer@gmail.com>
2024-11-23 13:42:03 +0530

    Add task component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Tabs from "./components/Tabs";
-import { loadActiveTab, saveActiveTab } from "./utils/localStorage";
+import {
+  loadActiveTab,
+  loadTasks,
+  saveActiveTab,
+  saveTasks,
+} from "./utils/localStorage";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [activeTab, setActiveTab] = useState(loadActiveTab());
+  const [tasks, setTasks] = useState(loadTasks());
 
   useEffect(() => {
     saveActiveTab(activeTab);
   }, [activeTab]);
+  useEffect(() => {
+    saveTasks(tasks);
+  }, [tasks]);
+
   return (
     <div className="app">
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <AddTask activeTab={activeTab} tasks={tasks} setTasks={setTasks} />
     </div>
   );
 };
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const AddTask = ({ activeTab, tasks, setTasks }) => {
+  const [taskText, setTaskText] = useState("");
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      status: "todo",
+    };
+    const updatedTasks = { ...tasks };
+    updatedTasks[activeTab].push(newTask);
+    setTasks(updatedTasks);
+    setTaskText("");
+  };
+  return (
+    <div className="add-task">
+      <form onSubmit={handleAddTask}>
+        <input
+          type="text"
+          placeholder={`Add a ${activeTab} task`}
+          value={taskText}
+          onChange={(e) => setTaskText(e.target.value)}
+          required
+        />
+        <button type="submit">Add task</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTask;
